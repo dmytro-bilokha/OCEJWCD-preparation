c850bb9ca008f553ff180734c1f96cbfe69d6282
Dmytro Bilokha <dmytro@posteo.net>
2016-05-16 15:59:11 +0300

    Added HelloServlet unit test

---
+++ helloservlet/src/test/java/bilokhado/helloservlet/HelloServletTest.java
@@ -0,0 +1,39 @@
+package bilokhado.helloservlet;
+
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static junit.framework.TestCase.fail;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class HelloServletTest {
+
+    @Test
+    public void shouldWriteHelloWorld() throws IOException {
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        StringWriter mockStrWriter = new StringWriter();
+        PrintWriter mockPrnWriter = new PrintWriter(mockStrWriter, true);
+        when(resp.getWriter()).thenReturn(mockPrnWriter);
+        String servletOutput;
+        try {
+            new HelloServlet().doGet(req, resp);
+        } catch (Exception e) {
+            fail(String.format("Call to HelloServlet.doGet() method has failed unexpectedly%n%s", e.getStackTrace()));
+        } finally {
+            servletOutput = mockStrWriter.toString();
+            mockPrnWriter.close();
+        }
+        verify(resp).getWriter();
+        assertThat(servletOutput, containsString("Hello World"));
+        assertThat(servletOutput, containsString("Servlet"));
+    }
+
+}
